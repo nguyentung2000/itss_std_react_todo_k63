eb2cf9b0a08b50210c8831c17ea756a7fe6e56be
nguyentung2000 <ntttung6100@gmail.com>
2022-05-31 13:41:06 +0700

    Create by other Component

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,13 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 /* 
   【inputコンポーネント】
 　・新しいTodoを作成するINPUTフィールドを作成するコンポーネント
 　・Enterをクリックされたら入力された文字を使って新しいTodoを作成する
 */
-function Input(  ) {
-
+function Input({ onAdd }) {
+  const [text, setText] = useState("");
+  const handleChange = (e) => setText(e.target.value);
+  const handleEnter = (e) => {
+    if (e.keyCode === 13) {
+      // e.preventDefault();
+      onAdd(text);
+      setText("");
+    }
+  };
   return (
     <div className="panel-block">
+      <input
+        type="text"
+        // placeholder="Nhap"
+        value={text}
+        onKeyDown={handleEnter}
+        onChange={handleChange}
+      />
     </div>
   );
 }
